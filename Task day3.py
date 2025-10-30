f75c2b0dd907ea1e87a4c9235320115e6cb5df8e
Abirami N <abiraminallamuthu96@gmail.com>
2025-10-30 12:27:09 +0530

    'data'

---
+++ Task day3.py
@@ -0,0 +1,135 @@
+print("add two numbers")
+a=10
+b=5
+a=int(input("enter your number"))
+b=int(input("enter your number"))
+print("addition",a+b)
+print()
+print("sub:",a-b)
+print()
+print("mul:",a*b)
+print()
+print("div:",a/b)
+print()
+print("mul:",a*b)
+modulas=a%b
+print("modulas:",a%b)
+print("pow:",a**2)
+print("cube",a**3)
+num1=int(input("enter your number"))
+num2=int(input("enter your number"))
+num3=int(input("enter your number"))
+
+print("ave",(num1+num2+num3)/3)
+x=10
+y=5
+z=y
+y=x
+x=z
+print("after swapping",x,y)
+print(type("hello"))
+age=29
+print("my age is",age)
+height=140.5
+print("my height is",height)
+f=10
+g=5
+print(f>g)
+print("check equal")
+a=5
+b=5
+print(a==b)
+print("equal or not")
+a=5
+b=5
+print(a!=b)
+print("total and average marks")
+sub1=int(input("enter a mark1:"))
+sub2=int(input("enter a mark2:"))
+sub3=int(input("enter a mark3:"))                        
+total=sub1+sub2+sub3
+average=total/3
+print("total marks",total)
+print("average marks",average)
+print("convert hours to minutes")
+hours=int(input("enter num of hours to be converted:"))
+min=hours*60           
+print(f"hourstomin:{hours}hours={min}minutes")
+print("calculte to simple interest")
+print("calculate simple interest")
+p=int(input("enter the princial of amount"))
+r=int(input("enter the rate of interest"))
+t=int(input( "enter the time"))     
+si=p*r*t/100
+print("the simple interest is:",si)
+print("area of a rectangle")
+length=int(input("enter the length of the rectangle"))
+breadth=int(input("enter the breadth of the rectangle"))
+area=length*breadth
+print("area of the rectangle is:",area)
+print("check positive or negative ")
+number=int(input("enter the number:"))
+if( number>0):
+ print("the number is positive")
+elif(number==0):
+ print("the number is zero")
+else:
+ print("the number is negative")
+print("calculate total cost")
+cost=int(input("enter the cost of item:"))
+num=int(input("enter the num of item:"))
+totalcost=cost*num 
+print("total cost of items:",totalcost)
+print("small calculator")
+num1=int(input("enter a number:"))
+num2=int(input("enter a number:"))
+g=num1+num2
+h=num1-num2
+i=num1*num2
+j=num1/num2
+print("addition=",g)
+print("subtracion=",h)
+print("multiplication=",i)
+print("division=",j)
+
+
+         
+                     
+           
+           
+
+
+
+
+
+
+
+         
+                     
+           
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
+
