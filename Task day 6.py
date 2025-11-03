d0c06d95caa2a6529784b5a910d706745316fce0
Abirami N <abiraminallamuthu96@gmail.com>
2025-11-03 17:00:45 +0530

    task

---
+++ Task day 6.py
@@ -0,0 +1,41 @@
+print("Numbers from 1 to 20")
+for i in range(1,21):
+    print(i)
+print()
+print("All even numbers 2 to 50")
+for i in range(2,51,2):
+   print(i)
+print()
+print("All odd numbers from 1 to 50 ")
+for i in range(1,51,2):
+    print(i)
+print()
+print("Square of numbers from 1 to 15")
+for i in range(1,16):
+    print(i*i)
+print()
+print("Cube of numbers from 1 to 10")
+for i in range(1,11):
+    print(i*i*i)
+print()
+print("print numbers from 10 down to 1 in reverse order")
+for i in range(10,0,-1):
+      print(i)
+print()
+print("Multilication of table 5")
+for i in range(1,11):
+      print(f" 5*{i} =5*{i}")
+print()
+print("All characters of a string one by one")
+name= "Abirami"
+for i in name:
+      print(i)
+print()
+print("Numbers divisible by 3 between 1 and 30")
+for i in range(1,31):
+    if i%3==0:
+        print(i)
+      
+      
+       
+    
